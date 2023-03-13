703418e6ce7fbd80821011286b5063134d31f4be
qiyue95525 <568982845@qq.com>
2023-03-13 22:20:27 +0800

    230313

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,11 +1,11 @@
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn) {
+    constructor(fn, public scheduler?) {
         this._fn = fn
     }
     run() {
         activeEffect = this
-        this._fn()
+        return this._fn()
     }
 }
 const targetMap = new Map()
@@ -29,12 +29,17 @@ export function trigger(target, key) {
     let depsMap = targetMap.get(target)
     let dep = depsMap.get(key)
     for (const effect of dep) {
-        effect.run()
+        if(effect.scheduler) {
+            effect.scheduler()
+        }else{
+            effect.run()
+        }
     }
 }
 
 let activeEffect;
-export function effect(fn) {
-    const _effect = new ReactiveEffect(fn)
+export function effect(fn, option: any = {}) {
+    const _effect = new ReactiveEffect(fn, option.scheduler)
     _effect.run()
+    return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,4 +14,37 @@ describe("effect", () => {
         user.age++
         expect(nextAge).toBe(12)
     })
+
+    it('should return runner when call effect', () => {
+        let foo = 10
+        const runner = effect(() => {
+            foo++;
+            return 'foo'
+        });
+        expect(foo).toBe(11);
+        const r = runner();
+        expect(foo).toBe(12);
+        expect(r).toBe("foo");
+    })
+
+    it('scheduler', () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner
+        })
+        const obj = reactive({ foo: 1 });
+        const runner = effect(() => {
+            dummy = obj.foo
+        },
+        { scheduler }
+        );
+        expect(scheduler).not.toHaveBeenCalled(); // 不会被调用
+        expect(dummy).toBe(1);
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1); // 会被调用1次
+        expect(dummy).toBe(1);
+        run();
+        expect(dummy).toBe(2);
+    })
 })
